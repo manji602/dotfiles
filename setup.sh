2cdc8a702dcb7049bd1907ac61bc3b1c5a3b4fc0
Jun HASHIMOTO <jun.hashimoto.0204@gmail.com>
2015-04-05 12:26:58 +0900

    fix setup script

--- setup.sh
+++ setup.sh
@@ -2,39 +2,12 @@
 
 cd "$(dirname "$0")"
 
-## setup git config
+TARGETS=(".gitconfig" ".gitignore" ".bashrc" ".screenrc" ".tmux.conf" ".emacs.d")
+RESOURCES=("git/.gitconfig" "git/.gitignore" "bash/.bashrc" "screen/.screenrc" "tmux/.tmux.conf" "emacs/.emacs.d")
 
-GIT_FILES=(".gitconfig" ".gitignore")
-
-for ((i = 0;i < ${#GIT_FILES[*]}; i++)) {
-    if [ ! -e $HOME/${GIT_FILES[$i]} ]; then
-        mv $HOME/${GIT_FILES[$i]} $HOME/"${GIT_FILES[$i]}.old"
+for ((i = 0;i < ${#TARGETS[*]}; i++)) {
+    if [ -f $HOME/${TARGETS[$i]} ]; then
+        mv -v $HOME/${TARGETS[$i]} $HOME/"${TARGETS[$i]}.old"
     fi
-    ln -Fisv "$PWD/git/${GIT_FILES[$i]}" $HOME
-}
-
-## setup bash config
-
-BASH_FILES=(".bashrc")
-
-for ((i = 0;i < ${#BASH_FILES[*]}; i++)) {
-    if [ ! -e $HOME/${BASH_FILES[$i]} ]; then
-        mv $HOME/${BASH_FILES[$i]} $HOME/"${BASH_FILES[$i]}.old"
-    fi
-    ln -Fisv "$PWD/bash/${BASH_FILES[$i]}" $HOME
-}
-
-## setup screen config
-
-## setup tmux config
-
-## setup emacs config
-
-EMACS_FILES=(".emacs.d")
-
-for ((i = 0;i < ${#EMACS_FILES[*]}; i++)) {
-    if [ ! -e $HOME/${EMACS_FILES[$i]} ]; then
-        mv $HOME/${EMACS_FILES[$i]} $HOME/"${EMACS_FILES[$i]}.old"
-    fi
-    ln -Fisv "$PWD/emacs/${EMACS_FILES[$i]}" $HOME
+    ln -Fisv "$PWD/${RESOURCES[$i]}" $HOME
 }
