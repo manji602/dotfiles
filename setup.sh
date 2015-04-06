dedeea4aadf73b1de40627d2b3c9d90f156272e4
Jun HASHIMOTO <jun.hashimoto.0204@gmail.com>
2015-04-06 09:00:50 +0900

    fix setup script

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ TARGETS=(".gitconfig" ".gitignore" ".bashrc" ".screenrc" ".tmux.conf" ".emacs.d"
 RESOURCES=("git/.gitconfig" "git/.gitignore" "bash/.bashrc" "screen/.screenrc" "tmux/.tmux.conf" "emacs/.emacs.d")
 
 for ((i = 0;i < ${#TARGETS[*]}; i++)) {
-    if [ -f $HOME/${TARGETS[$i]} ]; then
+    if [ -f $HOME/${TARGETS[$i]} -o -d $HOME/${TARGETS[$i]} ]; then
         mv -v $HOME/${TARGETS[$i]} $HOME/"${TARGETS[$i]}.old"
+	ln -Fisv "$PWD/${RESOURCES[$i]}" $HOME
     fi
-    ln -Fisv "$PWD/${RESOURCES[$i]}" $HOME
 }
