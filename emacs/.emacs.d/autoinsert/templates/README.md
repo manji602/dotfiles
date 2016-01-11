9710c1ba85158c676ba9eb409037a9612ac16436
Jun Hashimoto <jun.hashimoto.0204@gmail.com>
2016-01-11 23:14:34 +0900

    update autoinsert settings

--- emacs/.emacs.d/autoinsert/templates/README.md
+++ emacs/.emacs.d/autoinsert/templates/README.md
@@ -1,4 +1,4 @@
-sample
+%directory%
 ===
 
 ## Description
--- emacs/.emacs.d/conf/autoinsert-init.el
+++ emacs/.emacs.d/conf/autoinsert-init.el
@@ -1,12 +1,35 @@
+;; ==============================================================================
+;; autoinsert settings
+;; ==============================================================================
 (require 'autoinsert)
 
 (setq auto-insert-directory "~/.emacs.d/autoinsert/templates")
 
+(defvar template-list
+  '(
+    ("%directory%" . (lambda () (file-name-directory (buffer-file-name))))
+    ("%year%" . (lambda () (format-time-string "%Y" (current-time))))
+    ("%author%" . (lambda () "Jun Hashimoto"))
+    )
+)
+
+(defun autoinsert-template ()
+  (mapc #'(lambda(c)
+        (progn
+          (goto-char (point-min))
+          (replace-string (car c) (funcall (cdr c)) nil)))
+    template-list)
+  (goto-char (point-max))
+  (message "done."))
+
+;; NOTE:
+;; default template of LICENSE.txt is MIT
 (setq auto-insert-alist
       (nconc '(
-               ("README.md$" . ["README.md"])
-               ("LICENSE.txt$" . ["LICENSE.txt"])
+               ("README.md$" . ["README.md" autoinsert-template])
+               ("LICENSE.txt$" . ["LICENSE.txt" autoinsert-template])
                ) auto-insert-alist))
 (require 'cl)
 
 (add-hook 'find-file-not-found-hooks 'auto-insert)
+(setq auto-insert-query nil)
